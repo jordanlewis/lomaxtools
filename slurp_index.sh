d302884bf7614e1cf49982ad73d3c88a48cec9a7
Jordan Lewis <jordan@knewton.com>
2014-12-04 11:20:30 -0500

    Start slurper from page 1

--- slurp_index.sh
+++ slurp_index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir index
-curl "http://research.culturalequity.org/rc-b2/audio-ix-recording.jsp?d-446288-p=[8-385]" -o "index/p_#1.html" -H "Cookie:JSESSIONID=A80FA6648B6E3F664A965093032FF6AF"
+curl "http://research.culturalequity.org/rc-b2/audio-ix-recording.jsp?d-446288-p=[1-385]" -o "index/p_#1.html" -H "Cookie:JSESSIONID=A80FA6648B6E3F664A965093032FF6AF"
